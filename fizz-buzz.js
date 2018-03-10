ffb1b144ba31f97c629edc558992c7f3e6337597
ape <kgonzale@cbu.edu>
2018-03-10 10:48:27 -0600

    learning process.argv

---
+++ fizz-buzz.js
@@ -0,0 +1,16 @@
+let userInput = process.argv[2];
+
+const fizzBuzz = (userInput) => {
+    
+    if(userInput % 3 == 0){
+        console.log("fizz");
+    }
+    else if(userInput % 5 == 0){
+        console.log("buzz");
+    }
+    else if(userInput % 3 == 0 || userInput % 5 == 0){
+        console.log("fizz buzz");
+    }
+}
+
+fizzBuzz(userInput);

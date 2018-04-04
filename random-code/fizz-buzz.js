ef6b9715b3f94b0876b13d8fe992dca782d8c22d
Kevin Gonzalez <kgonzale@cbu.edu>
2018-04-03 20:27:36 -0500

    Update fizz-buzz.js

--- random-code/fizz-buzz.js
+++ random-code/fizz-buzz.js
@@ -2,13 +2,13 @@ let userInput = process.argv[2];
 
 const fizzBuzz = (userInput) => {
     
-    if(userInput % 3 == 0){
+    if(userInput % 3 === 0){
         console.log("fizz");
     }
-    else if(userInput % 5 == 0){
+    else if(userInput % 5 === 0){
         console.log("buzz");
     }
-    else if(userInput % 3 == 0 && userInput % 5 == 0){
+    else if(userInput % 3 === 0 && userInput % 5 === 0){
         console.log("fizz buzz");
     }
 }

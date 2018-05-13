a09cb40edd093fe5ff6ccba33a429d0983c9ca14
Kevin Gonzalez <kgonzale@cbu.edu>
2018-05-12 23:59:08 -0500

    fixed

--- random-code/savings.js
+++ random-code/savings.js
@@ -3,11 +3,20 @@ let moneyEarned = process.argv[2];
 if(moneyEarned < 0){
     console.log("Enter an input greater than 0");
 }
+else {
+    let checkings = .30;
+    checkings *= moneyEarned; 
+    console.log('Checkings fund: $' + checkings);
 
-let sixtyPercent = .60;
-sixtyPercent *= moneyEarned; 
-console.log('This amount goes into your checkings: $' + sixtyPercent);
+    let savings = .35;
+    savings *= moneyEarned;
+    console.log('Savings fund: $' + savings);
 
-let fortyPercent = .40;
-fortyPercent *= moneyEarned;
-console.log('This amount goes into your savings: $' + fortyPercent);
+    let emergency = .20;
+    emergency *= moneyEarned;
+    console.log('Emergency fund: $' + emergency);
+
+    let hobby = .15;
+    hobby *= moneyEarned;
+    console.log('Hobby fund: $' + hobby);
+}
